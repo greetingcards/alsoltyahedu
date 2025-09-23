697f943435d2511bb2f8a62f320665255af640d8
greetingcards <162377092+greetingcards@users.noreply.github.com>
2025-09-23 05:44:03 +0300

    Update newindex.js

--- js/newindex.js
+++ js/newindex.js
@@ -26,7 +26,7 @@ canv.drawImage(photo,0,0);
 function DrawText(canv,fillcolor) {
     canv.fillStyle = fillcolor; 
     canv.textBaseline = 'middle';
-    canv.font = " 150px 'Tajawal', sans-serif ";
+    canv.font = " 120px 'Tajawal', sans-serif ";
 }
 function DynamicText(canv,photo,clr,x,y) {
   document.getElementById('name').addEventListener('keyup', function() {
